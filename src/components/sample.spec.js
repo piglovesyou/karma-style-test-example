ded6cd08da9f226539b82503a22af0bce7b6ff46
piglovesyou <thepiglovesyou@gmail.com>
2016-06-29 22:22:19 +0900

    promiseComputedStyleOf

--- src/components/sample.spec.js
+++ src/components/sample.spec.js
@@ -2,30 +2,50 @@ const Path = require('path');
 const assert = require('power-assert');
 const {getDefaultStyle} = require('../tools');
 
-let windows = {};
+const base = Path.join(__dirname.slice('/'.length), Path.basename(__filename, '.spec.js'));
 
 describe("function-add", () => {
   beforeEach(() => {
-    const base = Path.join(__dirname.slice('/'.length), Path.basename(__filename, '.spec.js'));
-    for (let name of ['spec', 'test']) {
-      document.body.appendChild(document.createElement('h2')).textContent = name[0].toUpperCase() + name.slice(1);
-      const frame = document.body.appendChild(document.createElement('iframe'));
-      frame.contentDocument.body.innerHTML = window.__html__[base + '.' + name + '.html'];
-      windows[name] = frame.contentWindow;
-    }
+    let el;
+    while (el = document.body.firstChild) document.body.removeChild(el);
   });
   it("２つの引数の和を返すべし", () => {
-    assert.deepEqual(
-      renderedStyle('spec', '.sample'),
-      renderedStyle('test', '.sample'));
+    return Promise.all([
+      promiseComputedStyleOf(createElement(base + '.spec.html', '.sample')),
+      promiseComputedStyleOf(createElement(base + '.test.html', '.sample'))
+    ]).then(([spec, test]) => {
+      console.log(spec.getPropertyValue('padding'), test.getPropertyValue('padding'));
+    })
   });
 });
 
-function $(selector) {
-  return document.querySelector(selector);
+function promiseComputedStyleOf(element) {
+  const doc = element.ownerDocument;
+  const len = doc.querySelectorAll('link[rel="stylesheet"]').length;
+  return new Promise((resolve, reject) => {
+    let intervalTimer = setInterval(check, 40);
+    let timeoutTimer = setTimeout(timeout, 1000);
+    function check() {
+      if (doc.styleSheets.length >= len) {
+        clearTimer();
+        const style = element.ownerDocument.defaultView.getComputedStyle(element);
+        resolve(style);
+      }
+    }
+    function timeout() {
+      clearTimer();
+      reject(new Error('Stylesheet load timeout'));
+    }
+    function clearTimer() {
+      clearInterval(intervalTimer);
+      clearTimeout(timeoutTimer);
+    }
+  });
 }
 
-function renderedStyle(name, selector) {
-  const element = windows[name].document.querySelector(selector);
-  return Object.assign({}, windows[name].getComputedStyle(element), element.style)
+function createElement(htmlPath, selector) {
+  const sandbox = window.document.body.appendChild(document.createElement('iframe'));
+  const doc = sandbox.contentDocument;
+  doc.body.innerHTML = window.__html__[htmlPath];
+  return doc.querySelector(selector);
 }
