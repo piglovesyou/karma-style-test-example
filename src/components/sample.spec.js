ac3cdff83beee011470b6933eb5a5a9f2aa47e26
piglovesyou <thepiglovesyou@gmail.com>
2016-06-29 22:58:28 +0900

    StyleHelper

--- src/components/sample.spec.js
+++ src/components/sample.spec.js
@@ -1,51 +1,24 @@
 const Path = require('path');
 const assert = require('power-assert');
-const {getDefaultStyle} = require('../tools');
+const helper = require('../tools').get();;
 
 const base = Path.join(__dirname.slice('/'.length), Path.basename(__filename, '.spec.js'));
 
 describe("function-add", () => {
-  beforeEach(() => {
-    let el;
-    while (el = document.body.firstChild) document.body.removeChild(el);
-  });
+  beforeEach(() => helper.clean());
   it("２つの引数の和を返すべし", () => {
     return Promise.all([
-      promiseComputedStyleOf(createElement(base + '.spec.html', '.sample')),
-      promiseComputedStyleOf(createElement(base + '.test.html', '.sample'))
+      helper.promiseStyleWithHTML(window.__html__[base + '.spec.html'], '.sample'),
+      helper.promiseStyleWithHTML(`
+<link rel="stylesheet" href="base/src/components/sample.compiled.css" type="text/css" />
+<div class="sample">baa</div>
+<script src="base/src/components/sample.js"></script>
+        `, '.sample')
     ]).then(([spec, test]) => {
-      console.log(spec.getPropertyValue('padding'), test.getPropertyValue('padding'));
+      assert(Object.keys(spec).length, Object.keys(test).length);
+      for (let k of Object.keys(spec)) {
+        assert(spec[k] === test[k]);
+      }
     })
   });
 });
-
-function promiseComputedStyleOf(element) {
-  const doc = element.ownerDocument;
-  const len = doc.querySelectorAll('link[rel="stylesheet"]').length;
-  return new Promise((resolve, reject) => {
-    let intervalTimer = setInterval(check, 40);
-    let timeoutTimer = setTimeout(timeout, 1000);
-    function check() {
-      if (doc.styleSheets.length >= len) {
-        clearTimer();
-        const style = element.ownerDocument.defaultView.getComputedStyle(element);
-        resolve(style);
-      }
-    }
-    function timeout() {
-      clearTimer();
-      reject(new Error('Stylesheet load timeout'));
-    }
-    function clearTimer() {
-      clearInterval(intervalTimer);
-      clearTimeout(timeoutTimer);
-    }
-  });
-}
-
-function createElement(htmlPath, selector) {
-  const sandbox = window.document.body.appendChild(document.createElement('iframe'));
-  const doc = sandbox.contentDocument;
-  doc.body.innerHTML = window.__html__[htmlPath];
-  return doc.querySelector(selector);
-}
--- src/tools/index.js
+++ src/tools/index.js
@@ -1,2 +1,56 @@
-module.exports = {};
+class StyleHelper {
+  constructor() {
+    this.frames = [];
+  }
+  promiseStyleWithHTML(html, selector) {
+    const element = this.createElement(html, selector);
+    return this.promiseStyle(element);
+  }
+  promiseStyle(element) {
+    const doc = element.ownerDocument;
+    return this.waitForCSSLoad(doc)
+      .then(() => {
+        return doc.defaultView.getComputedStyle(element);
+      });
+  }
+  waitForCSSLoad(element) {
+    const doc = element.ownerDocument || element.defaultView ? element : null;
+    if (!doc) throw new Error('Invalid element was passed');
+    const len = doc.querySelectorAll('link[rel="stylesheet"]').length;
+    return new Promise((resolve, reject) => {
+      let intervalTimer = setInterval(check, 40);
+      let timeoutTimer = setTimeout(timeout, 1000);
+      if (check()) return;
+      function check() {
+        if (doc.styleSheets.length >= len) {
+          clearTimer();
+          resolve();
+          return true;
+        }
+      }
+      function timeout() {
+        clearTimer();
+        reject(new Error('Stylesheet load timeout'));
+      }
+      function clearTimer() {
+        clearInterval(intervalTimer);
+        clearTimeout(timeoutTimer);
+      }
+    });
+  }
+  createElement(html, selector) {
+    const frame = document.createElement('iframe');
+    this.frames.push(frame);
+    window.document.body.appendChild(frame);
+    const doc = frame.contentDocument;
+    doc.body.innerHTML = html;
+    return doc.querySelector(selector);
+  }
+  clean() {
+    for (let f of this.frames) {
+      f.parentNode.removeChild(f);
+    }
+  }
+}
 
+module.exports.get = () => new StyleHelper();
