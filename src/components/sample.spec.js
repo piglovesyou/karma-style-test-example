3fb6cce698bec92023edce99a81d617473d18d9e
piglovesyou <thepiglovesyou@gmail.com>
2016-06-30 10:32:29 +0900

    context.render and context.promiseStyles

--- src/components/sample.spec.js
+++ src/components/sample.spec.js
@@ -1,55 +1,34 @@
-const Path = require('path');
 const assert = require('power-assert');
-const helper = require('../tools').get();;
-
-// When you use html2js:
-// const base = Path.join(__dirname.slice('/'.length), Path.basename(__filename, '.spec.js'));
-// window.__html__[base + '.spec.html'];
+const {StyleHelper} = require('../tools');
 
 describe('sample component', () => {
-  beforeEach(() => helper.clean());
   it('Style of .spec.html must be the same as one using external stylesheet', () => {
-    return Promise.all([
-      helper.promiseStyleWithHTML(`
-
-<div class="sample" style="
-  border: 4px pink solid;
-  padding: 8px;
-  font-weight: bold;
-  ">baa</div>
-
-        `, '.sample', {viewportWidth: 600}),
-      helper.promiseStyleWithHTML(`
-
-<link rel="stylesheet" href="base/src/components/sample.compiled.css" type="text/css" />
-<div class="sample">baa</div>
-
-        `, '.sample', {viewportWidth: 600})
-    ]).then(([spec, test]) => {
-      assert.deepEqual(spec, test);
+    const context = new StyleHelper({
+      rootSelector: '.sample'
     });
 
-    // API design:
-    // const Helper = require('style-helper');
-    // const context = new Helper({
-    //   rootSelector: 'div',
-    //   viewportWidth: 600,
-    //   viewportHeight: 400,
-    // });
-    // const expectedRootElement = context.render(`
-    // <div class="sample" style="
-    //   border: 4px pink solid;
-    //   padding: 8px;
-    //   font-weight: bold;
-    //   ">baa</div>
-    //   `);
-    // const actualRootElement = context.render(`
-    // <link rel="stylesheet" href="base/src/components/sample.compiled.css" type="text/css" />
-    // <div class="sample">baa</div>
-    //   `);
-    // context.promiseStyles(expectedElement, actualElement)
-    //   .then(([expected, actual]) => {
-    //     assert.deepEqual(expected, actual);
-    //   });
+    const expectedRootElement = context.render(`
+    <div class="sample" style="
+      border: 4px pink solid;
+      padding: 8px;
+      font-weight: bold;
+      ">baa</div>
+      `);
+
+    const actualRootElement = context.render(`
+    <link rel="stylesheet" href="base/src/components/sample.compiled.css" type="text/css" />
+    <div class="sample">baa</div>
+      `);
+
+    return context.promiseStyles(expectedRootElement, actualRootElement)
+      .then(([expected, actual]) => {
+        assert.deepEqual(expected, actual);
+        context.clean();
+      });
   });
 });
+
+// When you use html2js and supposing there is "sample.spec.html":
+// const Path = require('path');
+// const base = Path.join(__dirname.slice('/'.length), Path.basename(__filename, '.spec.js'));
+// window.__html__[base + '.spec.html']; // will return html string
--- src/tools/index.js
+++ src/tools/index.js
@@ -1,66 +1,68 @@
 class StyleHelper {
-  constructor() {
+  constructor(options) {
+    this.options = Object.assign({
+      rootSelector: 'div',
+      viewportWidth: undefined,
+      viewportHeight: undefined,
+    }, options);
     this.frames = [];
   }
-  promiseStyleWithHTML(html, selector, options) {
-    options = options || {};
-    const element = this.createElement(html, selector, options);
-    return this.promiseStyle(element);
-  }
-  promiseStyle(element) {
-    const doc = element.ownerDocument;
-    return this.waitForCSSLoad(doc)
-      .then(() => {
-        return doc.defaultView.getComputedStyle(element);
-      });
-  }
-  waitForCSSLoad(element) {
-    const doc = element.ownerDocument || element.defaultView ? element : null;
-    if (!doc) throw new Error('Invalid element was passed');
-    const len = doc.querySelectorAll('link[rel="stylesheet"]').length;
-    return new Promise((resolve, reject) => {
-      let intervalTimer = setInterval(check, 40);
-      let timeoutTimer = setTimeout(timeout, 1000);
-      if (check()) return;
-      function check() {
-        if (doc.styleSheets.length >= len) {
-          clearTimer();
-          resolve();
-          return true;
-        }
-      }
-      function timeout() {
-        clearTimer();
-        reject(new Error('Stylesheet load timeout'));
-      }
-      function clearTimer() {
-        clearInterval(intervalTimer);
-        clearTimeout(timeoutTimer);
-      }
-    });
-  }
-  createElement(html, selector, options) {
-    options = options || {};
+  render(html) {
+    const {rootSelector, viewportWidth, viewportHeight} = this.options;
     const frame = document.createElement('iframe');
     this.frames.push(frame);
-
-    if (typeof options.viewportWidth === 'number') {
-      frame.style.width = options.viewportWidth + 'px';
+    if (typeof viewportWidth === 'number') {
+      frame.style.width = viewportWidth + 'px';
     }
-    if (typeof options.viewportHeight === 'number') {
-      frame.style.height = options.viewportHeight + 'px';
+    if (typeof viewportHeight === 'number') {
+      frame.style.height = viewportHeight + 'px';
     }
-
     window.document.body.appendChild(frame);
     const doc = frame.contentDocument;
     doc.body.innerHTML = html;
-    return doc.querySelector(selector);
+    return doc.querySelector(rootSelector);
+  }
+  promiseStyles(...elements) {
+    return Promise.all(elements.map(waitForCSSLoad))
+      .then(elements => elements.map(computedStyleOf));
   }
   clean() {
-    for (let f of this.frames) {
+    const {frames} = this;
+    this.frames = [];
+    for (let f of frames) {
       f.parentNode.removeChild(f);
     }
   }
 }
 
-module.exports.get = () => new StyleHelper();
+module.exports.StyleHelper = StyleHelper;
+
+function computedStyleOf(element) {
+  return element.ownerDocument.defaultView.getComputedStyle(element);
+}
+
+function waitForCSSLoad(element) {
+  const doc = element.ownerDocument || (element.defaultView ? element : null);
+  if (!doc) throw new Error('Invalid element was passed');
+  const len = doc.querySelectorAll('link[rel="stylesheet"]').length;
+  return new Promise((resolve, reject) => {
+    let intervalTimer = setInterval(check, 40);
+    let timeoutTimer = setTimeout(timeout, 1000);
+    if (check()) return;
+    function check() {
+      if (doc.styleSheets.length >= len) {
+        clearTimer();
+        resolve(element);
+        return true;
+      }
+    }
+    function timeout() {
+      clearTimer();
+      reject(new Error('Stylesheet load timeout'));
+    }
+    function clearTimer() {
+      clearInterval(intervalTimer);
+      clearTimeout(timeoutTimer);
+    }
+  });
+}
